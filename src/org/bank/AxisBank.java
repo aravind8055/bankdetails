607a164a0dafdb9b85ad842990c4400835ed7f01
aravind8055 <aravindkanna19@gmail.com>
2022-06-18 20:23:25 +0530

    save

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,21 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	@override
+	public void savind() {
+        System.out.println("my bank");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		AxisBank c1 = new AxisBank();
+		c1.savind();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("1.Saving");
+
+	}
+	public void fixed() {
+		System.out.println("2.Fixed");
+
+	}
+	
+	public static void main(String[] args) {
+		BankInfo c = new BankInfo();
+		
+	
+	}
+
+}
